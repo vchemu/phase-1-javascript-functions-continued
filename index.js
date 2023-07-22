1fe32bd9c3001ffce4b3f780d10c600bc06562fb
valerine <valerine.chemutai@student.moringaschool.com>
2023-07-22 14:08:23 +0300

    functions 2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("cycle");
+
+const mondayWork = function (action = "go to the office") {
+  return (`This Monday, I will ${action}.`);
+}
+mondayWork();
+mondayWork("study for my test");
+
+function wrapAdjective(flair = "*") {
+  return function (term = "special") {
+    return `You are ${flair}${term}${flair}!`
+  }
+}
+  wrapAdjective();
+  wrapAdjective("||", "great");
